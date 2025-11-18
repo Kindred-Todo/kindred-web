7790eec17a89ed804d7cea653f1b04be0396755e
Abhik Ray <abhikaboy@gmail.com>
2025-11-17 23:09:17 -0500

    buildwork 2

--- src/pages/landing/sections/VideoSection.tsx
+++ src/pages/landing/sections/VideoSection.tsx
@@ -6,7 +6,7 @@ export function VideoSection() {
   const videoHeight = scale(964)
   const sectionRef = useRef<HTMLElement>(null)
   const hasSnappedRef = useRef(false)
-  const timeoutRef = useRef<number>()
+  const timeoutRef = useRef<number | undefined>(undefined)
   const [isFixed, setIsFixed] = useState(false)
   
   useEffect(() => {
